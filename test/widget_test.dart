6671d27d8efa5139bb8d3d1deb207148b3615a53
Le Thanh <thanhln@goline.vn>
2021-07-06 17:35:21 +0700

    #1 fail unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,8 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
